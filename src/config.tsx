d62b4424fa41231430d88ec97a1c511f9eb404c0
alicelebi <ali1999_@hotmail.com>
2024-04-07 13:25:40 +0300

    first commit

---
+++ src/config.tsx
@@ -0,0 +1,21 @@
+import profileImage from '@/app/profile.webp';
+import { GithubIcon } from '@/components/icons/github';
+import { LinkedInIcon } from '@/components/icons/linkedin';
+
+export const CONFIG = {
+  name: 'Ali Rıza ÇELEBİ',
+  headshot: profileImage,
+  description: `Hi, I'm Ali Rıza ÇELEBİ. I work as a Frontend Developer. I'll be sharing blog posts and short notes here.`,
+  socials: [
+    {
+      platform: 'GitHub',
+      link: 'https://github.com/celebiali',
+      icon: <GithubIcon />,
+    },
+    {
+      platform: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/ali-r%C4%B1za-%C3%A7elebi-99a3a3181/',
+      icon: <LinkedInIcon />,
+    },
+  ],
+};
